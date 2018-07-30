3d0724ee6d1bf691b7e9da94757123d7ab767257
your_name <31688286+DongHaiFengCN@users.noreply.github.com>
2018-07-30 11:45:46 +0800

    修复异步网络加载图片不显示的问题

--- app/src/main/java/com/example/dong/test/MainActivity.java
+++ app/src/main/java/com/example/dong/test/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.dong.test;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import com.example.imageload.ImageLoader;
@@ -12,52 +9,46 @@ import com.example.imageload.MemoryAndDishCache;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    ImageLoader imageLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }
-
 
         final ImageView imageView = findViewById(R.id.image_ig);
 
-        //创建一个图片加载器
-        final ImageLoader imageLoader =  ImageLoader.getInstance();
+        //创建一个图片加载器，设置图片访问根路径
+        imageLoader =  ImageLoader.getInstance("https://cdn.pixabay.com/photo/2018/07/06/19/48/");
+
 
         //选择使用两级缓存方法
         MemoryAndDishCache memoryAndDishCache = new MemoryAndDishCache();
 
-        //设置图片质量
-        memoryAndDishCache.setQuality(100);
+        //设置图片质量，默认为100
+        memoryAndDishCache.setQuality(90);
 
-        //设置图片所要保存的文件夹的名称
+        //设置图片所要保存的文件夹的名称，不设置就是我的网名 DOAING
         memoryAndDishCache.setCacheName("test");
 
         imageLoader.setImageCache(memoryAndDishCache);
-
-      /*
-       //只使用sd卡缓存
-       DiskCache diskCache = DiskCache.getInstance();
-
-        imageLoader.setImageCache(diskCache);*/
-
-      /*
-      //只使用内存缓存
-       MemoryCache memoryCache = MemoryCache.getInstance();
-        imageLoader.setImageCache(memoryCache);*/
 
         findViewById(R.id.button_bt).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                imageLoader.displayImage("18030312320337431.jpg",imageView);
+                //图片名称
+                imageLoader.displayImage("charles-chaplin-3521070_1280.jpg",imageView);
 
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        imageLoader.close();
+        Log.e("DOAING","onDestroy");
+
     }
 }
